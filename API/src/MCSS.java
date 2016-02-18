51b2ad7c82295f986df4bff1c1287086fef996a6
Ricardo Vasquez <Ricardo Vasquez>
2016-02-18 15:59:55 -0500

    Add MCSS o(n) algorithm to API

--- API/src/MCSS.java
+++ API/src/MCSS.java
@@ -1,7 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class MCSS {
 
     public static int MCSS(int [] a) {
-        int max = 0, sum = 0, start = 0, end = 0, i=0;
+        int max = 0;
+        int sum = 0;
+        int start = 0;
+        int end = 0;
+        int i = 0;
 
         // Cycle through all possible end indexes.
         for (int j = 0; j < a.length; j++) {
@@ -9,14 +18,19 @@ public class MCSS {
             sum += a[j]; // No need to re-add all values.
             if (sum > max) {
                 max = sum;
-                start = i; // Although method doesn't return these
-                end = j; // they can be computed.
-            }
-            else if (sum < 0) {
-                i = j+1; // Only possible MCSSs start with an index >j.
+            } else if (sum < 0) {
+                i = j+1; // Only possible MCSSs start with an index greater than j
                 sum = 0; // Reset running sum.
             }
         }
         return max;
+    }
+
+    public static class MCSSTest {
+
+        @Test
+        public void test_MCSS() {
+            assertThat(20, is(MCSS(new int[]{5,7,-3,1,-11,8,12})));
+        }
     }
 }
